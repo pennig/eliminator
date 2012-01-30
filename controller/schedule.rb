39afa2069aa1cca78149249ffddb7773fcec0f4d
Paul Kehrer <paul.l.kehrer@gmail.com>
2012-01-30 16:21:09 -0600

    sexify schedule

--- controller/schedule.rb
+++ controller/schedule.rb
@@ -1,6 +1,6 @@
 class ScheduleController < Controller
     def index(season=current_season, week_type=current_week_type, week_number = current_week_number)
-        @title = "Schedule for #{season}"
+        @title = "Schedule for #{season} Week #{week_number}"
         dataset = Schedule.where(:season => season, :week_type => week_type)
         if week_number != "all"
             dataset = dataset.where(:week_number => week_number)
