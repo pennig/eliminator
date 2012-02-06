12036ef24b40423d6e4f69217e9a6c054095f58e
Paul Kehrer <paul.l.kehrer@gmail.com>
2012-02-05 20:22:56 -0600

    fix a few bugs on the statistics page related to season switching & links

--- controller/statistics.rb
+++ controller/statistics.rb
@@ -1,7 +1,6 @@
 class StatisticsController < Controller
     def index(off_def,season=2011)
         @season = season
-        @season_path = build_path
         if off_def == "offense"
             @stats = VTeamStatistics.where(:season => @season).order(:avg_points.desc)
             @stat_type = "offense"
@@ -9,6 +8,7 @@ class StatisticsController < Controller
             @stats = VOpponentStatistics.where(:season => @season).order(:avg_points.asc)
             @stat_type = "defense"
         end
+        @season_path = "/statistics/#{@stat_type}/"
     end
 end
 
