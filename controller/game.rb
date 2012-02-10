ccad9ac552a621f676d696d8a7286263b1d87631
Paul Kehrer <paul.l.kehrer@gmail.com>
2012-02-09 21:38:48 -0600

    remove season_path so we don't have a dropdown where we shouldn't

--- controller/game.rb
+++ controller/game.rb
@@ -2,7 +2,6 @@ class GameController < Controller
     def info(game_id)
         @schedule = Schedule.where(:game_id => game_id).first
         @season = @schedule.season
-        @season_path = build_path
         @results = @schedule.results
         @home_stats = GameStats.where(:game_id => game_id, :team_id => @schedule.home_team_id).first
         @away_stats = GameStats.where(:game_id => game_id, :team_id => @schedule.away_team_id).first
